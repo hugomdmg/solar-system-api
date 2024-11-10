6b67067f70db6834de984256272a87dc954f27ea
hugomdmg <hugomdmg@gmail.com>
2024-11-10 21:00:34 +0100

    multiplayer ships

--- index.js
+++ index.js
@@ -1,14 +1,36 @@
-import express, { urlencoded, json } from 'express'
-import cors from 'cors'
+const express = require('express')
+const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 3000
 
 app.use(cors())
-app.use(urlencoded({ extended: false }))
-app.use(json())
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
 
 app.get('/', (req, res) => {
-    res.send({ value: 'hello world' })
+    res.send({ value: 'hola mundo' })
+})
+
+let ships = []
+
+setInterval(()=>{
+    ships = []
+}, 3000)
+
+app.post('/ships', (req, res) => {
+
+    let inThelist = false
+    ships.map((ship, index) => {
+        if (req.body.id == ship.id) {
+            ships[index] = req.body
+            inThelist = true
+        }
+    })
+    if (!inThelist) {
+        ships.push(req.body)
+    }
+
+    res.send({ value: ships })
 })
 
 
