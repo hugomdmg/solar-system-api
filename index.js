58cc5e3af83cbd8f563b429ae628a1b7749b7d1e
hugomdmg <hugomdmg@gmail.com>
2024-11-14 17:16:54 +0000

    shots

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express()
 const port = process.env.PORT || 3000
 
 const corsOptions = {
-    origin: 'https://solar-system-explore.vercel.app',
+    // origin: 'https://solar-system-explore.vercel.app',
     methods: 'GET, POST',
     allowedHeaders: 'Content-Type'
 };
@@ -18,16 +18,28 @@ app.get('/', (req, res) => {
 })
 
 let ships = []
+let shots = []
 
-setInterval(()=>{
+
+setInterval(() => {
+    shots.forEach((shot) => {
+            shot.position.x = shot.position.x + (shot.velocity.x || 0)
+            shot.position.y = shot.position.y + (shot.velocity.y || 0)
+            shot.position.z = shot.position.z + (shot.velocity.z || 0)
+    })
+    if (shots.length > 5) {
+        shots.splice(0, shots.length - 5);
+    }
+}, 30)
+
+setInterval(() => {
     ships = []
-}, 3000)
+}, 3000);
 
 app.post('/ships', (req, res) => {
-
     let inThelist = false
     ships.map((ship, index) => {
-        if (req.body.id == ship.id) {
+        if (req.body.userId == ship.userId) {
             ships[index] = req.body
             inThelist = true
         }
@@ -37,6 +49,27 @@ app.post('/ships', (req, res) => {
     }
 
     res.send({ value: ships })
+})
+app.post('/shots', (req, res) => {
+   if(req.body != null){
+       let inThelist = false
+       shots.map((shot, index) => {
+           if (req.body.userId == shot.userId) {
+               shots[index] = req.body
+               inThelist = true
+           }
+       })
+       if (!inThelist) {
+           shots.push(req.body)
+       }
+
+   }
+
+    res.send({ value: shots });
+});
+
+app.get('/shots-data', (req, res) => {
+    res.send({ value: shots })
 })
 
 
