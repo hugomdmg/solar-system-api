a9899d6b3d52e4fc855ffd389b3f9a2430875357
hugomdmg <hugomdmg@gmail.com>
2024-11-16 23:27:21 +0000

    refactor

--- index.js
+++ index.js
@@ -11,23 +11,14 @@ const corsOptions = {
     allowedHeaders: 'Content-Type'
 };
 
-app.use(cors(corsOptions))
-app.use(express.urlencoded({ extended: false }))
-app.use(express.json())
-
-app.get('/', (req, res) => {
-    res.send({ value: 'solar system api' })
-})
-
 let ships = []
 let shots = []
 
-
 setInterval(() => {
     shots.forEach((shot) => {
-            shot.position.x = shot.position.x + (shot.velocity.x || 0)
-            shot.position.y = shot.position.y + (shot.velocity.y || 0)
-            shot.position.z = shot.position.z + (shot.velocity.z || 0)
+        shot.position.x = shot.position.x + (shot.velocity.x || 0)
+        shot.position.y = shot.position.y + (shot.velocity.y || 0)
+        shot.position.z = shot.position.z + (shot.velocity.z || 0)
     })
     if (shots.length > 5) {
         shots.splice(0, shots.length - 5);
@@ -38,8 +29,13 @@ setInterval(() => {
     ships = []
 }, 3000);
 
+app.use(cors(corsOptions))
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
 app.use('/images', express.static(path.join(__dirname, 'public')));
 
+app.get('/', (req, res) => { res.send({ value: 'solar system api' }) })
+app.get('/shots-data', (req, res) => { res.send({ value: shots }) })
 
 app.post('/ships', (req, res) => {
     let inThelist = false
@@ -52,30 +48,23 @@ app.post('/ships', (req, res) => {
     if (!inThelist) {
         ships.push(req.body)
     }
-
     res.send({ value: ships })
 })
+
 app.post('/shots', (req, res) => {
-   if(req.body != null){
-       let inThelist = false
-       shots.map((shot, index) => {
-           if (req.body.userId == shot.userId) {
-               shots[index] = req.body
-               inThelist = true
-           }
-       })
-       if (!inThelist) {
-           shots.push(req.body)
-       }
-
-   }
-
+    if (req.body != null) {
+        let inThelist = false
+        shots.map((shot, index) => {
+            if (req.body.id == shot.id) {
+                shots[index] = req.body
+                inThelist = true
+            }
+        })
+        if (!inThelist) {
+            shots.push(req.body)
+        }
+    }
     res.send({ value: shots });
 });
-
-app.get('/shots-data', (req, res) => {
-    res.send({ value: shots })
-})
-
 
 app.listen(port, () => { console.log(`Server: http://localhost:${port}`) })
